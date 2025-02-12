7b2f053cc29e1b1420278dbfcaf2a8cb23019ed5
AzisKasymov <abduazis.kasymov@alatoo.edu.kg>
2025-02-12 14:47:30 +0600

    first

--- user.py
+++ user.py
@@ -1,10 +1,23 @@
-class user:
-    def __init__(self, name, age):
+import random
+import string
+import re
+from datetime import datetime
+
+
+class User:
+    def __init__(self, user_id, name, surname, birthday):
+        self.user_id = user_id
         self.name = name
-        self.age = age
+        self.surname = surname
+        self.email = UserUtil.generate_email(name, surname, "example.com")
+        self.password = UserUtil.generate_password()
+        self.birthday = birthday
 
-    def get_name(self):
-        return self.name jndnfnj
+    def get_details(self):
+        return f"ID: {self.user_id}, Name: {self.name} {self.surname}, Email: {self.email}, Birthday: {self.birthday.strftime('%Y-%m-%d')}"
 
+    def get_age(self):
+        today = datetime.today()
+        return today.year - self.birthday.year - ((today.month, today.day) < (self.birthday.month, self.birthday.day))
 
 
